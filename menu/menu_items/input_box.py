04cd282a0b256d1ca34dd6ce576c70a527ff0ee0
Clivej13 <jamesclive13@gmail.com>
2023-05-17 09:21:18 +0100

    fixed bug, using menu_item_data

--- menu/menu_items/input_box.py
+++ menu/menu_items/input_box.py
@@ -14,13 +14,13 @@ class InputBox:
         self.hover_color = (200, 200, 200)
         self.clicked_color = (100, 100, 100)
         self.data = data
-        self.initial_text = data["menu_item_data"]
+        self.display_text = data["menu_item_value"]
         self.active = False
         self.logger = Logger()
 
     def draw(self, surface):
         pygame.draw.rect(surface, self.color, self.rect)
-        txt_surface = self.font.render(self.data["menu_item_data"], True, (0, 0, 0))
+        txt_surface = self.font.render(self.display_text, True, (0, 0, 0))
         # Blit the text.
         surface.blit(txt_surface, (self.rect.x + 5, self.rect.y + 5))
         # Blit the rect.
@@ -29,44 +29,48 @@ class InputBox:
         self.incorrect = False
         if not self.active:
             self.active = True
-        if self.data["menu_item_data"] == self.initial_text:
-            self.logger.log(self.data["menu_item_data"] + " match " + self.initial_text)
-            self.data["menu_item_data"] = ""
+        self.display_text = self.data["menu_item_value"]
         return [self.data, self.menu_name]
 
     def update(self):
+        if self.data["menu_item_value"] == "":
+            self.display_text = self.data["menu_item_data"]
+        else:
+            self.display_text = self.data["menu_item_value"]
         if self.active:
             self.color = (135, 206, 235)
+            self.display_text = self.data["menu_item_value"]
         elif not self.active:
             self.color = (255, 255, 255)
-            if self.data["menu_item_data"] == "":
-                self.data["menu_item_data"] = self.initial_text
             if self.rect.collidepoint(pygame.mouse.get_pos()):
                 self.color = self.hover_color
+            self.display_text = self.data["menu_item_data"]
         if self.incorrect:
             self.color = (255, 0, 51)
-            self.data["menu_item_data"] = self.initial_text
+            self.display_text = self.data["menu_item_data"]
             if self.rect.collidepoint(pygame.mouse.get_pos()):
                 self.color = self.hover_color
+        if self.data["menu_item_value"] == "\r":
+            self.data["menu_item_value"] = ""
 
     def deselect(self):
         if self.active:
             self.active = False
 
     def incorrect_value(self):
-        if self.data[1] == "Color":
+        if self.data["menu_item_validation"] == "Color":
             try:
-                list_rgb = self.data[0].split(",")
+                list_rgb = self.data["menu_item_value"].split(",")
                 if int(list_rgb[0]) > 0 or int(list_rgb[0]) < 255 and int(list_rgb[1]) > 0 or int(
                         list_rgb[1]) < 255 and int(list_rgb[2]) > 0 or int(list_rgb[2]) < 255:
                     return True
                 else:
-                    self.deselect()
                     self.incorrect = True
+                    self.data["menu_item_value"] = ""
                     return False
             except Exception as e:
                 self.logger.log("Exception")
-                self.deselect()
                 self.incorrect = True
+                self.data["menu_item_value"] = ""
                 return False
         return True
